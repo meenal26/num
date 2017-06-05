5affaf441041efc25b5c469e41cc372b66f7c8c5
meenal26 <meenalmeenu26@gmail.com>
2017-06-05 14:27:14 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,19 @@
+package abc;
+
+import java.util.Scanner;
+
+public class abcd {
+
+	public static void main(String[] args) {
+			int num1 = 0;
+			System.out.println("enter the number");
+			Scanner s=new Scanner(System.in);
+			int a=s.nextInt();
+					if(num1%2==0)
+				 System.out.println("Entered number is even");
+		     else
+		        System.out.println("Entered number is odd");
+					s.close();
+		}
+	}
+
